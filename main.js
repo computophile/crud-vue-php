68fb68f88741b432e5d3a29bbcbeb8323332658b
trtaseen <rashidthamidur@gmail.com>
2021-02-20 01:39:57 -0600

    Fetched data from the database and rendered with vue

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 var app = new Vue({
   el: "#app",
   data: {
-    errorMsg: false,
-    successMsg: false,
+    errorMsg: "",
+    successMsg: "",
     showAddModal: false,
     showEditModal: false,
     showDeleteModal: false,
-  }
+
+    users: [],
+    newUser: {name: "", email: "", phone: ""},
+    currentUser: {}
+  },
+  mounted: function(){
+    // when the app vue instance is created the mounted function will be exectud.
+    this.getAllUsers();
+  },
+  methods: {
+    getAllUsers(){
+      axios.get("http://localhost/crud-vue-php/process.php?action=read").then(
+        function(response){
+          if(response.data.error){
+            app.errorMsg = response.data.message;
+          }
+          else{
+            app.users = response.data.users;
+          }
+        }
+      )
+    },
+
+  },
 })
